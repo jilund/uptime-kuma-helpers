df4dccae21cf2bd1b51c56c5b5f12b354a00081c
jilund <jimmy.lundberg@qnet.se>
2024-08-01 20:09:47 +0200

    Add simple install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###############################################################################
+# Install scripts to /usr/local/bin/uptime-kuma-helpers
+###############################################################################
+
+mkdir -p /usr/local/bin/uptime-kuma-helpers
+
+# Copy scripts to /usr/local/bin/uptime-kuma-helpers
+
+cp ./scripts/* /usr/local/bin/uptime-kuma-helpers
